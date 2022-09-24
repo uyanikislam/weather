3acf821eb04dbf5238a8feb517365527e7f8f45e
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-09-24 21:47:48 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import axios from "axios";
-import {useEffect} from "react";
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import City from "./City";
 function App() {
-  const key= "a94ea8aacf81a7f4483794ee04feb324";
-  
-    useEffect(() => {
-      async function getApi() {
-        try {
-          const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${key}&units=metric`);
-          console.log(response);
-        } catch (error) {
-          console.error(error);
-        }
+  const key = "84a91b51124f2bc14e2104a40efa11c9";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
       }
-      getApi();
-    }, [])
-    return <div className="App"> </div>
-  }
+    }
+    getApi();
+  }, [search]);
+  console.log(search);
+  return (
+    <div className="App">
+      <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Placeholder"
+          className=" my-5 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "
+        />
+        {city && <City city={city} />}
+      </div>
+    </div>
+  );
+}
 
 export default App;
